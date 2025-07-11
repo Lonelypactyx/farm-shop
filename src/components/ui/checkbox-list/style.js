daf329d260c8c9aa35eac7075001fb064786bbdf
Lonelypactyx <41754883+Lonelypactyx@users.noreply.github.com>
2025-07-11 22:20:03 +0300

    Update style.js

--- src/components/ui/checkbox-list/style.js
+++ src/components/ui/checkbox-list/style.js
@@ -30,6 +30,7 @@ export const CheckboxLabel = styled.li`
       height: 22px;
       width: 22px;
       border: 1px solid rgba(0, 0, 0, 0.1);
+      background: url(${checkboxSelect}) no-repeat center;
       ${(props) =>
         props.$isChecked
           ? css`
